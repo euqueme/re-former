053838c8afe464c7a5eb2a4be26dcd0c055031f4
euqueme <euqueme@gmail.com>
2020-01-15 14:22:52 -0600

    Add Users model and controller

---
+++ db/migrate/20200115201029_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username, limit: 40
+      t.string :email, limit: 255
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
